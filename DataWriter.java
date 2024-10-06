962fb764a06ce3820080ae400524dcd546499ff0
tristenfornes <tfornes@email.sc.edu>
2024-10-06 14:27:14 -0400

    scrub datawriter

--- DataWriter.java
+++ DataWriter.java
@@ -1,3 +1,30 @@
+import java.util.List;
+
 public class DataWriter {
-    
+
+    public DataWriter() {
+        // Constructor
+    }
+
+    public boolean saveUsers(List<User> users) {
+        /**
+         * Stub for saving a list of User objects.
+         *
+         * @param users List of User objects to save.
+         * @return True if the operation was successful, False otherwise.
+         */
+        // Method stub - implementation to be added later
+        return false; // Placeholder return value
+    }
+
+    public boolean saveCourses(List<Course> courses) {
+        /**
+         * Stub for saving a list of Course objects.
+         *
+         * @param courses List of Course objects to save.
+         * @return True if the operation was successful, False otherwise.
+         */
+        // Method stub - implementation to be added later
+        return false; // Placeholder return value
+    }
 }
