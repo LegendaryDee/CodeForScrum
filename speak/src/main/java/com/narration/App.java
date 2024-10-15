793f0b0ed6d93e3a79f3086c76217ecc7d622156
tristenfornes <tfornes@email.sc.edu>
2024-10-15 09:05:33 -0400

    narration

---
+++ speak/src/main/java/com/narration/App.java
@@ -0,0 +1,13 @@
+package com.narration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Narrator.playSound("Je mapell Tristen!");
+    }
+}
