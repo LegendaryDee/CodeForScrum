8d8005f6a06b669d127244104a01cf11f879e218
DurantChristian <cdd7@email.sc.edu>
2024-10-22 09:34:50 -0400

    edited dataloader and datawriter

--- src/backEnd/DataWriter.java
+++ src/backEnd/DataWriter.java
@@ -1,38 +1,55 @@
 package backEnd;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-public class DataWriter {
+/**
+ * The DataWriter class is responsible for writing flashcard data to a JSON file.
+ * It takes a list of Flashcard objects and converts them into JSON format before
+ * saving them to the file.
+ */
+public class DataWriter extends DataConstants {
 
-    // Mock methods to save data to a file or database
+   /** 
+    * Path to the JSON file where flashcard data will be written.
+    */
+   private static final String FILE_PATH = "data.json";
 
-    public boolean saveUsers(List<User> users) {
-        // Assume we are writing to a file or database.
-        // Replace with actual file writing logic.
-        try {
-            // FileWriter or database operation
-            return true;  // return true if successful
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;  // return false if something goes wrong
+   /**
+    * Writes a list of flashcards to the JSON file specified in FILE_PATH.
+    * This method converts each Flashcard object into a JSON representation and writes
+    * the entire list of flashcards as a JSON array to the file.
+    *
+    * @param flashcards The list of Flashcard objects to be written to the file.
+    */
+  
+// (@SuppressWarnings("unchecked")) to get rid of the warnings.
+@SuppressWarnings("unchecked")
+public static void writeFlashcards(List<Flashcards> flashcards) {
+        // Create a JSON array to hold flashcard data
+        JSONArray flashcardList = new JSONArray();
+
+        // Convert each Flashcard object to a JSON object
+        for (Flashcards flashcard : flashcards) {
+            JSONObject flashcardDetails = new JSONObject();
+
+            flashcardDetails.put("word", flashcard.getWord());
+            flashcardDetails.put("translation", flashcard.getTranslation());
+            flashcardDetails.put("phrase", flashcard.getPhrase());
+
+            // Add the flashcard JSON object to the array
+            flashcardList.add(flashcardDetails);
         }
-    }
 
-    public static boolean saveCourses(List<Course> courses) {
-        // Similar to saveUsers, this would write course data.
-        try {
-            // FileWriter or database operation
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+        // Write the JSON array to the file
+        try (FileWriter file = new FileWriter(FILE_PATH)) {
+            file.write(flashcardList.toJSONString());  // Write JSON data to file
+            file.flush();  // Ensure all data is written
+        } catch (IOException e) {
+            e.printStackTrace();  // Handle errors in writing to the file
         }
-    }
-
-    public static void saveUsers() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'saveUsers'");
-    }
-
-    // Potentially add more methods for saving other types of data
+   }
 }
